d0290c48a9df51c4e1dff5615fd29ab9534b6539
Ashish Sahu <ashishsahu10428@gmail.com>
2021-07-30 23:09:05 +0530

    js

---
+++ Project-10 js-Basics/Level2/index.js
@@ -0,0 +1,19 @@
+var n1 = 12;
+var n2 = 23;
+
+console.log(n1+n2);
+
+var st1 = "THis is string 1";
+var st2 = "This is string 2";
+console.log(st1+" "+st2);
+
+var marks = {
+    ashish: 20,
+    sahu: 12,
+    ishu: 16,
+}
+
+console.log(marks);
+
+var test = true;
+console.log(test);
