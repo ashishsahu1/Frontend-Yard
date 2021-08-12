54d86d306db8b39dbf5de1bfa02c3875c3929971
Ashish Sahu <ashishsahu10428@gmail.com>
2021-08-12 17:32:17 +0530

    basic of js

--- Project-10 js-Basics/Level2/index.js
+++ Project-10 js-Basics/Level2/index.js
@@ -1,19 +1,31 @@
+
+//Numbers
 var n1 = 12;
 var n2 = 23;
-
 console.log(n1+n2);
 
+//Strings
 var st1 = "THis is string 1";
 var st2 = "This is string 2";
 console.log(st1+" "+st2);
 
+//Objects
 var marks = {
     ashish: 20,
     sahu: 12,
     ishu: 16,
 }
-
 console.log(marks);
 
+//Boolean
 var test = true;
 console.log(test);
+
+//Arrays : COllection of any datatype
+arr = [1,2,3,4,5]
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[2])
+console.log(arr[3])
+console.log(arr[4])
